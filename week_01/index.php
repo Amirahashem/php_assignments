ed8d91f18dd72092db9a8656edfce44c04586813
Amira Hashem <121437507+Amirahashem@users.noreply.github.com>
2026-08-11 20:13:50 +0300

    Add files via upload

---
+++ week_01/index.php
@@ -0,0 +1,52 @@
+<!-- Q1 -->
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?php echo 'UTF-8'; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?='My First PHP Page'?></title>
+  </head>
+  <body>
+    <?php 
+    /*
+      This is my first PHP assignment.
+      I am learning PHP comments.
+    */
+    ?>
+    <div><?php echo 'We Love'?></div>
+    <div><?php print 'Elzero Channel' ?></div>
+  </body>
+</html>
+
+<!-- Q2 -->
+ <?php
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
+
+?>
+
+<!-- Q3 -->
+<?php
+/* My Application
+ Version 1.0
+ Created By Elzero */
+?>
+
+<!-- Q4-->
+<?php
+
+// Correct
+ // ## First Comment
+
+ // Correct
+# // # Second Comment
+
+// Incorrect:  Nested comments are not allowed
+/* /* /* Third Comment */
+
+// Correct
+////// Fourth Comment
+
+?>
